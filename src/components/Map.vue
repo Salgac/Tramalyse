4761bb356c7374e271c19b108b8d9e6ab58a4036
Dominik <domco.salgovic@gmail.com>
2021-10-03 22:56:38 +0200

    Implement pseudorandom gpx line color generation

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -40,6 +40,7 @@ export default defineComponent({
     return {
       map: null,
       gpxLayers: [],
+      colorOffset: 0,
     };
   },
   mounted() {
@@ -68,7 +69,7 @@ export default defineComponent({
           shadowUrl: "",
         },
         polyline_options: {
-          color: "orange",
+          color: this.generateUniqueColor(),
         },
       });
 
@@ -105,6 +106,25 @@ export default defineComponent({
 
       newGpx.addTo(this.map);
       this.gpxLayers.push(newGpx);
+    },
+    generateUniqueColor() {
+      var h = 35 + this.colorOffset,
+        s = 100,
+        l = 50;
+      this.colorOffset += 65;
+      this.colorOffset %= 360;
+
+      //convet to hex
+      l /= 100;
+      const a = (s * Math.min(l, 1 - l)) / 100;
+      const f = (n) => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color)
+          .toString(16)
+          .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+      };
+      return `#${f(0)}${f(8)}${f(4)}`;
     },
   },
   components: {
