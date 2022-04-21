969eb190717ad9b9050e8d2077abd57d4e6e5fff
Dominik <domco.salgovic@gmail.com>
2022-04-21 14:42:34 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view id="page" />
+  <body>
+    <header>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </header>
+
+    <router-view id="page" />
+
+    <footer>
+      <p>&copy; 2021-{{ year }} Dominik Šalgovič | Faculty of Informatics and Information Technologies of Slovak University of Technology</p>
+    </footer>
+  </body>
 </template>
 
 <script lang="ts">
 export default {
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
   beforeCreate() {
     document.querySelector("body")?.setAttribute("style", "margin: 0px");
   },
@@ -29,11 +41,12 @@ export default {
   padding-top: 0px;
 }
 
-#nav {
+header {
   padding: 10px;
   margin-bottom: 5px;
   background-color: #333;
   text-align: center;
+  min-height: 20px;
 
   a {
     font-weight: bold;
@@ -47,10 +60,24 @@ export default {
   }
 }
 
-#page {
-  width: 95%;
-  max-width: 1920px;
-  min-width: 375px;
-  margin: auto;
+body {
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  #page {
+    width: 95%;
+    max-width: 1920px;
+    min-width: 375px;
+    margin: auto;
+  }
+}
+
+footer {
+  min-height: 50px;
+  background-color: #333;
+  color: white;
+  margin-top: auto;
 }
 </style>
